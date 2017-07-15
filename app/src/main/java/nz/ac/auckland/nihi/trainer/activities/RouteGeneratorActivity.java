104c56526a2dac207de2113612d3924f7c64baac
Aaron Zhong <aaron_zhong2b@hotmail.com>
2017-07-15 19:24:30 +1200

    Add action button for route generator

---
+++ app/src/main/java/nz/ac/auckland/nihi/trainer/activities/RouteGeneratorActivity.java
@@ -0,0 +1,16 @@
+package nz.ac.auckland.nihi.trainer.activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nz.ac.auckland.nihi.trainer.R;
+
+public class RouteGeneratorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_generator);
+    }
+}
